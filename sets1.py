02983db71e5972f20e704d2e190fe1dddfc6aa5d
“yatinsolanki” <yatinsolanki98@gmail.com>
2021-12-31 07:00:20 +0530

    sets

---
+++ sets1.py
@@ -0,0 +1,21 @@
+thislist = {"apple", "banana", "cherry"}
+
+print(thislist)
+
+#the set list is unodered.
+#refresh the screen to check the result.
+
+
+myset1 = {"name"}
+
+print("myset1")
+
+print(type(myset1))
+
+myset2 ={"name":"YATIN"}
+
+print(myset2)
+
+print(type(myset2))
+
+#duplictes are not allowed in sets
