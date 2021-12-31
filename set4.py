8b4198510b1a5bf6fc143ec958da6d172d0c6854
“yatinsolanki” <yatinsolanki98@gmail.com>
2021-12-31 07:29:01 +0530

    union, multiple function

---
+++ set4.py
@@ -0,0 +1,19 @@
+friends = {"ravi", "rohit", "rahul"}
+
+"""
+for variable in set/list/dict/tuple
+"""
+
+#for x in friends:
+
+print(f"hello,{friends}how are you")
+
+#union
+
+mca1 = {"a"," b", "c", "d"}
+
+mca2 = {"c", "f", "g", "h"}
+
+mcateam = mca1.union(mca2)
+
+print(mcateam)
