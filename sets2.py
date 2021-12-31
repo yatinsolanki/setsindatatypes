d4b233d307e96a5551004ae8a9b3faf2c449b671
“yatinsolanki” <yatinsolanki98@gmail.com>
2021-12-31 07:13:20 +0530

    lenght,constructor,remove

---
+++ sets2.py
@@ -0,0 +1,26 @@
+#lenght in sets
+
+student = {"RONAK", "ROM", "SHYAM", "RAVI", "RAHUL", "ROHIT"}
+
+print(student)
+
+print(len(student))
+
+print(f"the lenght of the student list is {len(student)}")
+
+#set contructor
+
+student =set(("RONAK", "ROM", "SHYAM", "RAVI", "RAHUL", "ROHIT", "AAKASH"))
+
+print(student)
+
+#remove element 
+student.remove("RAVI")
+
+print(student)
+
+#discart element
+
+#student.discart("POOJA")
+
+#print(student)
